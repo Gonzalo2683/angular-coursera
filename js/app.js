bf5e5826128884333611974b9a1d498827975263
Gonzalo Guevara <gonzarock@yahoo.com>
2017-04-20 16:05:55 -0300

    Se agrego ejemplos de inteccion de servicios en angular

--- js/app.js
+++ js/app.js
@@ -9,4 +9,22 @@
         $scope.name = 'Gonza';
         $scope.apellido = 'Guevara';
     })
+    .controller('nombreCalculado', function($scope){
+        $scope.name = '';
+        $scope.largoNombre = function(){
+            return this.name.length;
+        }
+    })
+    .controller('inyeccionDependencia', function($scope, $filter){ //se iyectan servicios de angular
+        $scope.name = '';
+        $scope.upper = function(){
+            var upCase = $filter('uppercase');
+            $scope.name = upCase($scope.name);
+        }
+    })
+    .controller('otraInyeccion', funcOtraInyeccion)
+    funcOtraInyeccion.$inject = ['$scope', '$filter']
+    function funcOtraInyeccion($scope, $filter){
+        $scope.name = 'Intectado';
+    }
 })();
